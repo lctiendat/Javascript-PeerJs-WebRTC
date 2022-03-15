24b26c44d51ceccf684f21ade132568f5895f297
lctiendat <lctiendat@lctiendats-MacBook-Pro.local>
2022-03-15 14:41:36 +0700

    callvideo

---
+++ app.js
@@ -0,0 +1,127 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getFirestore, addDoc, collection } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA3EoOnj0Ka42iul1ltA8kGXRNnNppg0l8",
+    authDomain: "callvideo-bb197.firebaseapp.com",
+    projectId: "callvideo-bb197",
+    storageBucket: "callvideo-bb197.appspot.com",
+    messagingSenderId: "1001320655290",
+    appId: "1:1001320655290:web:74a93aa7b704fe43f99583"
+};
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+var peer = new Peer();
+const myCamera = $('#my-camera');
+const friendCamera = $('#friend-camera');
+const server = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] }
+const randomID = Math.floor(Math.random() * 1000000000000000);
+var seftRemote = null
+var friendRemote = null
+const PC = new RTCPeerConnection(server);
+var resource = null
+var video = null
+
+function userMedia() {
+    return navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true
+    })
+}
+
+function showCamera(whereRemote, stream) {
+    whereRemote.get(0).srcObject = stream;
+}
+
+
+
+$('#creat-hangout').click(e => {
+    $('.show-camera').addClass('d-block').removeClass('d-none')
+    $('.show-room').addClass('d-block').removeClass('d-none')
+    $('.action').addClass('d-none').removeClass('d-block')
+    userMedia().then(stream => {
+        stream.getVideoTracks().length == 0 ? $('.list-action li:first-child i').addClass('bi-camera-video-off turnon-camera') :
+            $('.list-action li:first-child i').addClass('bi-camera-video turnoff-camera')
+        stream.getAudioTracks().length == 0 ? $('.list-action li:last-child i').addClass('bi-volume-mute turnon-volume') :
+            $('.list-action li:last-child i').addClass('bi-volume-down turnoff-volume')
+        showCamera(myCamera, stream);
+    })
+})
+
+$('#join-hangout').click(e => {
+    Swal.fire({
+        title: 'Nhập ID phòng muốn tham gia',
+        input: 'text',
+        inputAttributes: {
+            autocapitalize: 'on'
+        },
+        showCancelButton: true,
+        confirmButtonText: 'Tham gia',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $('.show-camera').addClass('d-block').removeClass('d-none')
+            $('.show-camera-friend').removeClass('d-none').addClass('d-block')
+            $('.action').addClass('d-none').removeClass('d-block')
+            const idRoom = result.value
+            userMedia().then(stream => {
+                showCamera(myCamera, stream);
+                const call = peer.call(idRoom, stream);
+                call.on('stream', friendStream => {
+                    showCamera(friendCamera, friendStream);
+                })
+            })
+        }
+    })
+})
+
+peer.on('call', call => {
+    userMedia().then(stream => {
+        window.localStream = stream;
+        $('.show-camera-friend').removeClass('d-none').addClass('d-block')
+        call.answer(stream);
+        call.on('stream', friendStream => {
+            showCamera(friendCamera, friendStream);
+        })
+    })
+})
+
+$(document).on('click', '.turnoff-camera', (e) => {
+    $('.turnoff-camera').addClass('bi-camera-video-off turnon-camera').removeClass('bi-camera-video turnoff-camera')
+    userMedia().then(stream => {
+        stream.getVideoTracks()[0].enabled = false;
+        myCamera.src = ''
+        myCamera.get(0).srcObject = null
+            // showCamera(myCamera, stream);
+    })
+})
+
+$(document).on('click', '.turnon-camera', (e) => {
+    $('.turnon-camera').addClass('bi-camera-video turnoff-camera').removeClass('bi-camera-video-off turnon-camera')
+
+    userMedia().then(stream => {
+        stream.getVideoTracks()[0].enabled = true;
+        showCamera(myCamera, stream);
+    })
+})
+
+$(document).on('click', '.turnoff-volume', (e) => {
+    $('.turnoff-volume').addClass('bi-volume-mute turnon-volume').removeClass('bi-volume-down turnoff-volume')
+    userMedia().then(stream => {
+        stream.getAudioTracks()[0].enabled = false;
+        showCamera(myCamera, stream);
+    })
+})
+
+$(document).on('click', '.turnon-volume', (e) => {
+    $('.turnon-volume').addClass('bi-volume-down turnoff-volume').removeClass('bi-volume-mute turnon-volume')
+    userMedia().then(stream => {
+        stream.getAudioTracks()[0].enabled = true;
+        showCamera(myCamera, stream);
+    })
+})
+
+peer.on('open', id => {
+    $('#roomID').append(id)
+})
